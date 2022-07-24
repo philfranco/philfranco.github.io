fd987c95fd4b1e777b5323cea85ec7a44cdf8d26
Philip Franco <pcfranco05@gmail.com>
2022-07-23 22:18:47 -0400

    Removing Junk

--- javaScript/boxSizeAdjust.js
+++
@@ -1,7 +0,0 @@
-var input = document.querySelector('input')
-var footer = document.querySelector('footer')
-
-input.addEventListener('change', function(){
-  footer.style.setProperty('--textBox_h', window.innerHeight)
-  footer.style.setProperty('--textBox_w', window.innerWidth)
-})
